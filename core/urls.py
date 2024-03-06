965e47eea9e31d0d25f11496e74bec3b6f3aee33
imad <eddinemsg@gmail.com>
2024-03-06 16:03:23 +0100

    Template organization, second phase

--- core/urls.py
+++ core/urls.py
@@ -5,27 +5,35 @@ from core import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("register/", views.register, name="register"),
-
-    # Client
-    path("client_profile/", views.client_edit_profile, name="client_profile"),
-    path("client_settings/", views.client_settings, name="client_settings"),
-    path("client_dashboard/", views.client_dashboard, name="client_dashboard"),
-    path("client_view_accounts/", views.client_view_accounts, name="client_view_accounts"),
-    path("client_create_transfer/", views.client_create_transfer, name="client_create_transfer"),
-    path("client_create_account/", views.create_account, name="account_create"),
-
-    # Staff
-    path("staff_create_account/", views.staff_create_account, name="staff_create_account"),
-    path("staff_view_accounts/", views.staff_view_accounts, name="staff_view_accounts"),
-    path("staff_view_clients/", views.staff_view_clients, name="staff_view_clients"),
-
     # Main
     path("", views.index, name="index"),
     path("register/", views.register, name="register"),
-    path("products/", views.products, name="products"),
-    path("about/", views.about_us, name="about_us"),
-    path("contact/", views.contact_us, name="contact_us"),
-
+    path("main_products/", views.main_products, name="main_products"),
+    path("main_about_us/", views.main_about_us, name="main_about_us"),
+    path("main_contact_us/", views.main_contact_us, name="main_contact_us"),
+    # Staff
+    path(
+        "staff_create_account/",
+        views.client_create_account,
+        name="staff_create_account",
+    ),
+    path("staff_view_accounts/", views.staff_view_accounts, name="staff_view_accounts"),
+    path("staff_view_clients/", views.staff_view_clients, name="staff_view_clients"),
+    # Client
+    path("client_profile/", views.client_profile, name="client_profile"),
+    path("client_settings/", views.client_settings, name="client_settings"),
+    path("client_dashboard/", views.client_dashboard, name="client_dashboard"),
+    path(
+        "client_view_accounts/", views.client_view_accounts, name="client_view_accounts"
+    ),
+    path(
+        "client_create_transfer/",
+        views.client_create_transfer,
+        name="client_create_transfer",
+    ),
+    path(
+        "client_create_account/",
+        views.client_create_account,
+        name="client_create_account",
+    ),
 ]
--- core/views.py
+++ core/views.py
@@ -1,29 +1,26 @@
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.core.mail import EmailMessage
 from django.db import transaction
 from django.shortcuts import redirect, render
-from django.contrib.auth import login
-from core.forms import CustomerRegistrationForm
 
-from .forms import AccountForm, ContactForm, EditProfileForm, KYCForm, TransferForm
 from .models import Account, Customer, Ledger
 
 
 # Main
-
 def index(request):
     return render(request, "core/main/index.html")
 
 
-def products(request):
+def main_products(request):
     return render(request, "core/main/main_products.html")
 
 
-def about_us(request):
+def main_about_us(request):
     return render(request, "core/main/main_about_us.html")
 
 
-def contact_us(request):
+def main_contact_us(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -40,10 +37,131 @@ def contact_us(request):
                 reply_to=[email],  # Email from the form to get back to
             ).send()
 
-            return redirect("core/main/main_contact_us.html")
+            return redirect("core:main_contact_us")
     else:
         form = ContactForm()
     return render(request, "core/main/main_contact_us.html", {"form": form})
+
+
+# Staff
+
+
+@login_required
+def staff_view_clients(request):
+    # Retrieve all accounts in the system
+    customers = Customer.objects.all()
+    context = {"customers": customers}
+    return render(request, "core/client/staff_view_clients.html", context)
+
+
+@login_required
+def staff_view_accounts(request):
+    accounts = Account.objects.all()
+    context = {"accounts": accounts}
+    return render(request, "core/account_staff.html", context)
+
+
+# Client
+
+
+@login_required
+def client_profile(request):
+    if request.method == "POST":
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("core:client_profile")
+    else:
+        form = EditProfileForm(instance=request.user)
+
+    return render(
+        request, "core/client/client_profile.html", {"user": request.user, "form": form}
+    )
+
+
+@login_required
+def client_settings(request):
+    return render(request, "core/client/client_settings.html")
+
+
+@login_required
+def client_dashboard(request):
+    user = request.user
+
+    # Retrieve the latest transfers associated with the user's accounts
+    latest_transfers = Ledger.objects.filter(account__user=user).order_by("-date")[:10]
+
+    context = {
+        "user": user,
+        "latest_transfers": latest_transfers,
+    }
+
+    return render(request, "core/client/client_dashboard.html", context)
+
+
+@login_required
+def client_view_accounts(request):
+    # Retrieve accounts associated with the logged-in user
+    user = request.user
+    accounts = Account.objects.filter(user=user)
+    context = {"accounts": accounts}
+    return render(request, "core/client_view_accounts.html", context)
+
+
+@login_required
+@transaction.atomic
+def client_create_transfer(request):
+    if request.method == "POST":
+        form = TransferForm(request.POST)
+        if form.is_valid():
+            source_account = form.cleaned_data["source_account"]
+            destination_account = form.cleaned_data["destination_account"]
+            amount = form.cleaned_data["amount"]
+            description = form.cleaned_data["description"]
+
+            # Deduct amount from source account
+            source_account.balance -= amount
+            source_account.save()
+
+            # Add amount to destination account
+            destination_account.balance += amount
+            destination_account.save()
+
+            # Create ledger entries
+            Ledger.objects.create(
+                account=source_account, amount=-amount, description=description
+            )
+            Ledger.objects.create(
+                account=destination_account, amount=amount, description=description
+            )
+
+            # Redirect to dashboard or success page
+            return redirect("core:client_create_transfer")
+    else:
+        form = TransferForm()
+    return render(request, "core/client/client_create_transfer.html", {"form": form})
+
+
+@login_required
+def client_create_account(request):
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            # Get the current user
+            user = request.user
+            name = form.cleaned_data["name"]
+            # Create a new instance of the Account model with form data and user
+            new_account = Account(name=name, user=user)
+            # Save the new account to the database
+            new_account.save()
+            # Optionally, redirect the user to a success page or perform other actions
+            return redirect("core:client_create_account")
+    else:
+        form = AccountForm()
+    return render(request, "core/client/client_create_account.html", {"form": form})
+
+
+# Unsorted
 
 
 def register(request):
@@ -80,118 +198,3 @@ def register(request):
         "registration/register.html",
         {"user_form": user_form, "kyc_form": kyc_form},
     )
-
-
-@login_required
-def dashboard(request):
-    user = request.user
-
-    # Retrieve the latest transfers associated with the user's accounts
-    latest_transfers = Ledger.objects.filter(account__user=user).order_by("-date")[:10]
-
-    context = {
-        "user": user,
-        "latest_transfers": latest_transfers,
-    }
-
-    return render(request, "core/dashboard.html", context)
-
-
-@login_required
-@transaction.atomic
-def create_transfer(request):
-    if request.method == "POST":
-        form = TransferForm(request.POST)
-        if form.is_valid():
-            source_account = form.cleaned_data["source_account"]
-            destination_account = form.cleaned_data["destination_account"]
-            amount = form.cleaned_data["amount"]
-            description = form.cleaned_data["description"]
-
-            # Deduct amount from source account
-            source_account.balance -= amount
-            source_account.save()
-
-            # Add amount to destination account
-            destination_account.balance += amount
-            destination_account.save()
-
-            # Create ledger entries
-            Ledger.objects.create(
-                account=source_account, amount=-amount, description=description
-            )
-            Ledger.objects.create(
-                account=destination_account, amount=amount, description=description
-            )
-
-            # Redirect to dashboard or success page
-            return redirect("core:dashboard")
-    else:
-        form = TransferForm()
-    return render(request, "core/account_transfer.html", {"form": form})
-
-
-@login_required
-def create_account(request):
-    if request.method == "POST":
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            # Get the current user
-            user = request.user
-            name = form.cleaned_data["name"]
-            # Create a new instance of the Account model with form data and user
-            new_account = Account(name=name, user=user)
-            # Save the new account to the database
-            new_account.save()
-            # Optionally, redirect the user to a success page or perform other actions
-            return redirect("core:account_create")
-    else:
-        form = AccountForm()
-    return render(request, "core/account_create.html", {"form": form})
-
-
-@login_required
-def account_customer(request):
-    # Retrieve accounts associated with the logged-in user
-    user = request.user
-    accounts = Account.objects.filter(user=user)
-    context = {"accounts": accounts}
-    return render(request, "core/account_customer.html", context)
-
-
-@login_required
-def edit_profile(request):
-    if request.method == "POST":
-        form = EditProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect("core:edit_profile")
-    else:
-        form = EditProfileForm(instance=request.user)
-
-    return render(
-        request, "core/edit_profile.html", {"user": request.user, "form": form}
-    )
-
-
-@login_required
-def settings(request):
-    return render(request, "core/settings.html")
-
-
-# STAFF:
-
-
-@login_required
-def account_staff(request):
-    accounts = Account.objects.all()
-    context = {"accounts": accounts}
-    return render(request, "core/account_staff.html", context)
-
-
-@login_required
-def customer_staff(request):
-    # Retrieve all accounts in the system
-    customers = Customer.objects.all()
-    context = {"customers": customers}
-    return render(request, "core/customer_staff.html", context)
